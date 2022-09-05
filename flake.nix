02a777354d2391236498612ebeab1ebdf5e68bea
lep <lep.delete@googlemail.com>
2022-09-05 18:25:03 +0200

    Nix and python module

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+    inputs = {
+	nixpkgs.url = "github:NixOS/nixpkgs";
+	flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+	flake-utils.lib.eachDefaultSystem (system:
+	    let pkgs = import nixpkgs { inherit system; };
+		py = pkgs.python3Packages;
+
+		module = py.buildPythonPackage {
+		    pname = "jassbot";
+		    version = "1.0.0";
+		    src = self;
+
+		    doCheck = false;
+
+		    propagatedBuildInputs = [
+			py.markdown
+			py.flask
+		    ];
+		};
+		mypython = pkgs.python3.withPackages(_: [ module ]);
+	    in {
+		defaultPackage = module;
+		devShell = pkgs.mkShell {
+		    buildInputs = [
+			pkgs.python3
+			module
+		    ];
+		};
+	    }
+	);
+}
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+        name='jassbot blueprint',
+        version="1.0.0",
+        packages=[
+            'jassbot',
+            'jassbot.templates.jassbot',
+            'jassbot.static.jassbot',
+            ],
+        include_package_data=True,
+        install_requires=[
+            'flask',
+            'markdown',
+            ],
+        )
