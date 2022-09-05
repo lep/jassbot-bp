cac5dd4370ca988d31404692a2e82dfa389eedea
lep <lep.delete@googlemail.com>
2022-09-05 18:37:59 +0200

    Add package

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
 		};
 		mypython = pkgs.python3.withPackages(_: [ module ]);
 	    in {
+		packages.jassbot-bp = module;
 		defaultPackage = module;
 		devShell = pkgs.mkShell {
 		    buildInputs = [
