2adf55affd813cd3971abf2e227ec490f28f4701
lep <lep.delete@googlemail.com>
2024-03-11 14:58:14 +0100

    Provide pyproject.toml

--- setup.py
+++ setup.py
@@ -3,17 +3,11 @@
 from setuptools import setup
 
 setup(
-        name='jassbot-bp',
-        version="1.0.0",
-        packages=[
-            'jassbot',
-            'jassbot.templates.jassbot',
-            'jassbot.static.jassbot',
-            ],
-        include_package_data=True,
-        install_requires=[
-            'flask',
-            'markdown',
-            'requests',
-            ],
-        )
+      name='jassbot-bp',
+      packages=[
+          'jassbot',
+          'jassbot.templates.jassbot',
+          'jassbot.static.jassbot',
+      ],
+      include_package_data=True,
+      )
