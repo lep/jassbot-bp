1a6ae32b1c42462e12a5c667010c59336a63a2a9
lep <lep.delete@googlemail.com>
2024-03-24 12:04:42 +0100

    Display parsed query ast

--- jassbot/static/jassbot/preview.js
+++ jassbot/static/jassbot/preview.js
@@ -1,6 +1,37 @@
 function setup(){
-    var bar = document.getElementById("search")
-    var display = document.getElementById("results")
+    const bar = document.getElementById("search")
+    const display = document.getElementById("result")
+    const results = display.querySelector('.results')
+    const queryExplainer = display.querySelector('#queryexplainer')
+
+    const explain_tag = {
+        EmptyQuery(x) {
+            return "<empty>"
+        },
+        NameQuery(x) {
+            return `name(${x.contents})`
+        },
+        ReturnQuery(x) {
+            return `return-type(${x.contents})`
+        },
+        ParamQuery(x) {
+            return `takes(${x.contents.join(", ")})`
+        },
+        ExtendsQuery(x) {
+            return `extends(${x.contents})`
+        },
+        SumQuery(x) {
+            return `combined-score(${x.contents.map(explain_query).join(", ")})`
+        },
+        MinQuery(x) {
+            return `best-of(${x.contents.map(explain_query).join(", ")})`
+        }
+    }
+
+    function explain_query(ast) {
+        const tag = ast.tag
+        return explain_tag[tag](ast)
+    }
 
     function after_timeout(){
         var value = bar.value
@@ -11,13 +42,20 @@ function setup(){
 
         var on_load = function(){
             var json = JSON.parse(this.responseText)
+
+            queryExplainer.innerHTML = ""
+            // explain_query(json.queryParsed, queryExplainer)
+            queryExplainer.textContent = explain_query(json.queryParsed)
+
+
+
             results.innerHTML = ""
-            json.forEach(function(v){
+            json.results.forEach(function(v){
                 var div = document.createElement("div")
                 var code = tokenize(v, "code", jass_tokens)
                 div.setAttribute("class", "result")
                 div.appendChild(code)
-                display.appendChild(div)
+                results.appendChild(div)
             })
         }
 
@@ -26,8 +64,6 @@ function setup(){
         req.addEventListener("load", on_load)
         req.open("GET", $SCRIPT_ROOT+"search/api/"+value)
         req.send()
-
-
     }
 
     var timer = false
