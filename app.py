0a447a8e39a85b29e98e84d66ed6798e73d5ad76
lep <lep.delete@googlemail.com>
2025-05-27 20:18:39 +0200

    Standarize app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 import jassbot
 
 app = Flask(__name__)
 app.config.from_prefixed_env()
-app.register_blueprint(jassbot.bp)
+app.config["SERVER_NAME"] = "localhost:5000"
+app.register_blueprint(jassbot.mk_bp(url_prefix="/jassbot"))
 
 if __name__ == '__main__':
     app.run("localhost", 5000, debug=True)
-
