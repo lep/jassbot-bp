aa855d67de106037986dd321fe3037ff03effb32
lep <lep.delete@googlemail.com>
2025-10-14 16:20:56 +0200

    Simplyfy unpackPhase as we simply use minify for templates aswell

--- flake.nix
+++ flake.nix
@@ -22,9 +22,7 @@
 
           postUnpack = ''
             minify --recursive -o source/jassbot/static/ source/jassbot/static/jassbot
-            for template in source/jassbot/templates/jassbot/*.html.j2; do
-              minify --type handlebars  -o "$template" "$template"
-            done
+            minify --recursive --type mustache -o source/jassbot/templates/ source/jassbot/templates/jassbot
 
             for js in source/jassbot/static/jassbot/*.js; do
               closure-compiler --js "$js" | sponge "$js"
